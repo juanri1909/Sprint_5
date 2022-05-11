97b05cb2e5bdc9c950fcf173799bb2c0bfa99079
juanri1909 <jrigarci1909@gmail.com>
2022-05-11 18:11:16 +0200

    Delete S05T02N02RiosJuan directory

--- S05T02N02RiosJuan/src/main/java/cat/itacademy/barcelonactiva/rios/juan/s05/t022/S05T02N02RiosJuan/model/repository/TiradaRepositorio.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.rios.juan.s05.t022.S05T02N02RiosJuan.model.repository;
-
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import cat.itacademy.barcelonactiva.rios.juan.s05.t022.S05T02N02RiosJuan.model.domain.Tirada;
-
-public interface TiradaRepositorio extends MongoRepository<Tirada, Integer>{
-		Tirada findByidpartida(int idpartida);
-}
